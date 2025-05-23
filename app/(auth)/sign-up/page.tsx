4a61933405ad74d67c0478b445a4c04c942751df
manavnotop <manuagarwal1443@gmail.com>
2025-05-23 15:02:21 +0530

    Add: Auth pages ui

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "@/components/AuthForm";
+
+export default function SignUp(){
+  return(
+    <div>
+      <AuthForm type="sign-up" />
+    </div>
+  )
+}
---
+++ components/FormField.tsx
@@ -0,0 +1,28 @@
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { FormControl, FormDescription, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>
+  name: Path<T>
+  label: string
+  placeholder?: string
+  type?: 'text' | 'email' | 'password' | 'file'
+}
+
+export default function FormField<T extends FieldValues>({ control, name, label, placeholder, type='text'}: FormFieldProps<T>){
+  return(
+    <div>
+      <Controller name={name} control={control} render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{ label }</FormLabel>
+          <FormControl>
+            <Input className="input" placeholder={ placeholder } type={ type } {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+      />
+    </div>
+  )
+}
