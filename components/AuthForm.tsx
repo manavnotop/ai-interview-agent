c07cfe7f30bab6c37783e9066e0145fe3f386d61
manavnotop <manuagarwal1443@gmail.com>
2025-05-28 15:06:28 +0530

    Add: Sign in and sign out logic and protect routes

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -11,6 +11,9 @@ import Link from "next/link";
 import { toast } from "sonner";
 import FormField from "./FormField";
 import { useRouter } from "next/navigation";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/firebase/client";
+import { signIn, signUp } from "@/lib/actions/auth.action";
 
 const authFormSchema = (type: FormType) => {
   return z.object({
@@ -33,14 +36,42 @@ export default function AuthForm({ type }: { type: FormType }){
     },
   })
  
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try{
       if( type === 'sign-up'){
-        console.log('Sign Up', values);
-        toast.success('Account Created Successfully. Please Sign In');
+        const { name, email, password } = values;
+        const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+        
+        const result = await signUp({
+          uid: userCredentials.user.uid,
+          name: name!,
+          email,
+          password
+        });
+
+        if(!result?.success){
+          toast.error(result?.message);
+          return;
+        }
+
+        toast.success('Account Created Successfully. Please Sign In.');
         router.push('/sign-in');
       }
       else{
+        const { email, password } = values;
+        const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+
+        const idToken = await userCredentials.user.getIdToken();
+
+        if(!idToken){
+          toast.error('Sign in failed');
+          return;
+        }
+
+        await signIn({
+          email, idToken
+        })
+
         toast.success('Sign In Successfully.');
         router.push('/');
       }
@@ -65,7 +96,7 @@ export default function AuthForm({ type }: { type: FormType }){
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
             {!isSign && (
-              <FormField control={form.control} name="name" label='name' placeholder='Your name' />
+              <FormField control={form.control} name="name" label='Name' placeholder='Your name' />
             )}
             <FormField control={form.control} name="email" label='Email' placeholder='Your email address' />
             <FormField control={form.control} name="password" label='Password' placeholder='Enter your password' type="password" />
