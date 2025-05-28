505951d07b9510e028cace420c4fa068a9a32655
manavnotop <manuagarwal1443@gmail.com>
2025-05-28 16:31:14 +0530

    Add: Interview page ui

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,10 @@
+import Agent from "@/components/Agent";
+
+export default function Interview(){
+  return(
+    <>
+      <h3>Interview Generation</h3>
+      <Agent userName='you' userId='user1' type='generate'/>
+    </>
+  )
+}
